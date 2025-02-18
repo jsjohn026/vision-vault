f4cd57efdab5fb55eb5f99bfdd7ae71f54337158
Jasmine John <jsjohn026@gmail.com>
2025-02-17 23:29:22 -0600

    adjust PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config"
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.use(express.json())
 app.use(cors())
 
